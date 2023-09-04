0502eeb6947f330068095b0428577e5cc44cd5d5
ji_rxin <1148769892@qq.com>
2023-09-04 08:29:00 +0800

    测试 classload

---
+++ javase_rt/lang/src/main/java/classloader/custom/MyClassLoader.java
@@ -0,0 +1,13 @@
+package classloader.custom;
+
+/**
+ *
+ */
+public class MyClassLoader extends ClassLoader{
+
+	@Override
+	protected Class<?> findClass(String name) throws ClassNotFoundException {
+
+		return super.findClass(name);
+	}
+}
