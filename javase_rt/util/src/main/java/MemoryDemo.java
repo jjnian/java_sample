1864fbd2c9443b29688639d2eee921630be69de5
ji_rxin <1148769892@qq.com>
2023-09-04 08:30:09 +0800

    测试 classload

--- javase_rt/util/src/main/java/MemoryDemo.java
+++ javase_rt/util/src/main/java/MemoryDemo.java
@@ -6,7 +6,7 @@ import java.util.LinkedList;
 /**
  *
  */
-public class TEST {
+public class MemoryDemo {
 
 	public static void main(String[] args) {
 		MemoryMXBean memoryMXBean = ManagementFactory.getMemoryMXBean();
